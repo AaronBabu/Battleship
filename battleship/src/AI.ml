148715aefdf0977c6f8f4be260be7aa78beee5b7
Aaron Babu <ab2458@cornell.edu>
2023-03-22 18:43:34 -0400

    Added dif symbol for every ship and fixed ship overlap bug

--- battleship/src/AI.ml
+++ battleship/src/AI.ml
@@ -29,6 +29,8 @@ let randomgrid =
 
 let () = Random.self_init ()
 
+let used_coords = ref []
+
 let ywing = {ship = Ship1; length = 2}
 let xwing = {ship = Ship2; length = 2}
 let mfalcon = {ship = Ship3; length = 3}
@@ -46,17 +48,18 @@ let rec generate_ship_coords len coord dir acc =
   | 0, _, _ -> acc
   | _, (c, n), 'h' when Char.code c < Char.code 'J' ->
       let new_coord = (Char.chr (Char.code c + 1), n) in
-      if Char.code (fst new_coord) <= Char.code 'J' then
+      if Char.code (fst new_coord) <= Char.code 'J' && not (List.mem new_coord !used_coords) then
         generate_ship_coords (len - 1) new_coord 'h' (new_coord :: acc)
       else
         acc
   | _, (c, n), 'v' when n < 10 ->
       let new_coord = (c, n + 1) in
-      if snd new_coord <= 10 then
+      if snd new_coord <= 10 && not (List.mem new_coord !used_coords) then
         generate_ship_coords (len - 1) new_coord 'v' (new_coord :: acc)
       else
         acc
   | _, _, _ -> acc
+  
 
 
 let rec place_ship (ship:ships) =
@@ -69,17 +72,38 @@ let rec place_ship (ship:ships) =
   else
     place_ship ship
 
+
+ (** in
+  let coords = generate_ship_coords (length - 1) (col, row) direction [] in
+  let all_coords = (col, row) :: coords in
+  List.iter (fun coord -> used_coords := coord :: !used_coords) all_coords;*)
+
+
+let change coords = List.iter (fun x-> used_coords := x :: !used_coords) coords
 let ywingCoords = place_ship ywing
-
+let _ = change ywingCoords
 let xwingCoords = place_ship xwing
+let _ = change xwingCoords
 let mfalconCoords = place_ship mfalcon
+let _ = change mfalconCoords
 let delta7Coords = place_ship delta7
+let _ = change delta7Coords
 let stardestroyerCoords = place_ship stardestroyer
+let _ = change stardestroyerCoords
 
-let place_ship_on_grid grid ship_coords =
+let place_ship_on_grid grid ship_coords ship=
   let update_grid row col = 
     let row_arr = List.nth grid row in
-    row_arr.(col) <- "#"
+    if ship.ship = Ship1 then
+      row_arr.(col) <- "#"
+    else if ship.ship = Ship2 then
+      row_arr.(col) <- "@"
+    else if ship.ship = Ship3 then
+      row_arr.(col) <- "$"
+    else if ship.ship = Ship4 then
+      row_arr.(col) <- "%"
+    else if ship.ship = Ship5 then
+      row_arr.(col) <- "~"
   in
   let rec loop coords =
     match coords with
@@ -92,12 +116,12 @@ let place_ship_on_grid grid ship_coords =
   in
   loop ship_coords
 
-
-let _ = place_ship_on_grid randomgrid ywingCoords
-let _ = place_ship_on_grid randomgrid xwingCoords
-let _ = place_ship_on_grid randomgrid mfalconCoords
-let _ = place_ship_on_grid randomgrid delta7Coords
-let _ = place_ship_on_grid randomgrid stardestroyerCoords
+let _ = used_coords
+let _ = place_ship_on_grid randomgrid ywingCoords ywing
+let _ = place_ship_on_grid randomgrid xwingCoords xwing
+let _ = place_ship_on_grid randomgrid mfalconCoords mfalcon
+let _ = place_ship_on_grid randomgrid delta7Coords delta7
+let _ = place_ship_on_grid randomgrid stardestroyerCoords stardestroyer
 
 
 
