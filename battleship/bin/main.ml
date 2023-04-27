93a0710106ab8da894d21bb173358cf5ee498424
Caitlyn Cahill <crc258@cornell.edu>
2023-04-26 20:40:01 -0400

    Print AI board then player board

--- battleship/bin/main.ml
+++ battleship/bin/main.ml
@@ -14,27 +14,30 @@ let player_board () =
   let new_grid = List.map (fun row -> Array.copy row) player_grid in
   new_grid
 
-let rec play_game grid string =
+let rec play_game grid grid2 string =
   try
     let move2 = parse string in
     new_turn grid move2;
     print_endline "\n AI board \n";
     print_column_label grid;
     print_grid grid 1;
+    print_endline "\n Player board \n";
+    print_column_label grid2;
+    print_grid grid2 1;
     print_endline "\n Choose a target: \n";
     let line = read_line () in
-    play_game grid line
+    play_game grid grid2 line
   with _ ->
     print_endline "\n Choose a target: \n";
     let line = read_line () in
-    play_game grid line
+    play_game grid grid2 line
 
 let main () =
   print_endline "\n AI board \n";
   let grid1 = reset_board () in
+  let grid2 = player_board () in
   print_endline "Choose a target:";
   let line = read_line () in
-  play_game grid1 line
-  
+  play_game grid1 grid2 line
 
 let () = main ()
