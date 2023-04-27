00c7b4ab6b77bfad42f304752a52353abe796f41
Caitlyn Cahill <crc258@cornell.edu>
2023-04-26 20:34:32 -0400

    Created helper functions that let player place ships

--- battleship/bin/main.ml
+++ battleship/bin/main.ml
@@ -1,5 +1,6 @@
-(** [play_game f] starts the game with state [g]. *)
 open Lib
+(** [play_game f] starts the game with state [g]. *)
+
 open Battleship
 open AI
 open Command
@@ -9,21 +10,31 @@ let reset_board () =
   let new_grid = List.map (fun row -> Array.copy row) randomgrid in
   new_grid
 
-let rec play_game grid string= 
+let player_board () =
+  let new_grid = List.map (fun row -> Array.copy row) player_grid in
+  new_grid
+
+let rec play_game grid string =
   try
-      let move2 = parse string in 
-      new_turn grid move2;
-      print_column_label grid;
-      print_grid grid 1;
-      print_endline "\n Choose a target: \n";
-      let line = (read_line ()) in play_game grid line
-  with _ -> 
-      print_endline "\n Choose a target: \n";
-      let line = (read_line ()) in play_game grid line
+    let move2 = parse string in
+    new_turn grid move2;
+    print_endline "\n AI board \n";
+    print_column_label grid;
+    print_grid grid 1;
+    print_endline "\n Choose a target: \n";
+    let line = read_line () in
+    play_game grid line
+  with _ ->
+    print_endline "\n Choose a target: \n";
+    let line = read_line () in
+    play_game grid line
 
 let main () =
-  let grid = reset_board () in
+  print_endline "\n AI board \n";
+  let grid1 = reset_board () in
   print_endline "Choose a target:";
-  let line = (read_line ()) in play_game grid line
+  let line = read_line () in
+  play_game grid1 line
+  
 
 let () = main ()
--- battleship/src/battleship.ml
+++ battleship/src/battleship.ml
@@ -1,3 +1,5 @@
+open AI
+
 (* Define the grid dimensions *)
 let num_rows = 10
 let num_cols = 10
@@ -45,7 +47,7 @@ let rec print_grid array (row_idx : int) =
       print_endline "";
       print_grid t (row_idx + 1)
 
-let grid =
+let player_grid =
   [
     [| " "; " "; " "; " "; " "; " "; " "; " "; " "; " " |];
     [| " "; " "; " "; " "; " "; " "; " "; " "; " "; " " |];
@@ -60,11 +62,98 @@ let grid =
   ]
 
 (* Call the print_grid function to print the grid *)
-let _ = print_column_label grid
-let _ = print_grid grid 1
+let _ = print_column_label player_grid
+let _ = print_grid player_grid 1
 
 let make_play =
-  print_column_label grid;
-  print_grid grid 1;
-  grid
+  print_column_label player_grid;
+  print_grid player_grid 1;
+  player_grid
 
+let playership1 = { ship = Ship1; length = 2 }
+let playership2 = { ship = Ship2; length = 3 }
+let playership3 = { ship = Ship3; length = 3 }
+let playership4 = { ship = Ship4; length = 4 }
+let playership5 = { ship = Ship5; length = 5 }
+
+type direction =
+  | Left
+  | Right
+  | Up
+  | Down
+
+let inverse_char_code (index : int) : char =
+  let code = index + 65 in
+  Char.chr code
+
+let place_shiplength_2 (direction : direction) ((char, int) : char * int) :
+    char * int =
+  match (direction, char, int) with
+  | Left, _, _ ->
+      let new_char_int = Char.code char - 2 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Right, _, _ ->
+      let new_char_int = Char.code char + 2 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Up, _, _ ->
+      let new_int = int + 2 in
+      (char, new_int)
+  | Down, _, _ ->
+      let new_int = int - 2 in
+      (char, new_int)
+
+let place_shiplength_3 (direction : direction) ((char, int) : char * int) :
+    char * int =
+  match (direction, char, int) with
+  | Left, _, _ ->
+      let new_char_int = Char.code char - 3 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Right, _, _ ->
+      let new_char_int = Char.code char + 3 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Up, _, _ ->
+      let new_int = int + 3 in
+      (char, new_int)
+  | Down, _, _ ->
+      let new_int = int - 3 in
+      (char, new_int)
+
+let place_shiplength_4 (direction : direction) ((char, int) : char * int) :
+    char * int =
+  match (direction, char, int) with
+  | Left, _, _ ->
+      let new_char_int = Char.code char - 4 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Right, _, _ ->
+      let new_char_int = Char.code char + 4 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Up, _, _ ->
+      let new_int = int + 4 in
+      (char, new_int)
+  | Down, _, _ ->
+      let new_int = int - 4 in
+      (char, new_int)
+
+let place_shiplength_5 (direction : direction) ((char, int) : char * int) :
+    char * int =
+  match (direction, char, int) with
+  | Left, _, _ ->
+      let new_char_int = Char.code char - 5 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Right, _, _ ->
+      let new_char_int = Char.code char + 5 in
+      let new_char = inverse_char_code new_char_int in
+      (new_char, int)
+  | Up, _, _ ->
+      let new_int = int + 5 in
+      (char, new_int)
+  | Down, _, _ ->
+      let new_int = int - 5 in
+      (char, new_int)
