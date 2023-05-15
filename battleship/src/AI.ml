dbd699c86c2799d7806e88726b90161e7fba0b8d
Aiman Mobhani <amm492@cornell.edu>
2023-05-14 22:24:10 -0400

    ai phrases and ai.ml

--- battleship/src/AI.ml
+++ battleship/src/AI.ml
@@ -48,8 +48,8 @@ let is_occupied grid (col, row) =
   if row >= 1 && row <= 10 && char_code >= 65 && char_code <= 74 then
     let row_arr = List.nth grid (row - 1) in
     row_arr.(char_code - 65) = "#"
-  else
-    false
+  else false
+
 let coord () = (letter_randomizer (), number_randomizer ())
 
 let rec generate_ship_coords len coord dir acc =
@@ -74,19 +74,24 @@ let rec place_ship grid (ship : ships) ship_coords =
   let coords =
     (col, row) :: generate_ship_coords (length - 1) (col, row) direction []
   in
-  if List.length coords = length
-      && List.for_all (fun c -> not (is_occupied grid c)) coords
-      && not (List.exists (fun coord -> List.mem coord ship_coords) coords)
-  then
-    coords
-  else
-    place_ship grid ship ship_coords
+  if
+    List.length coords = length
+    && List.for_all (fun c -> not (is_occupied grid c)) coords
+    && not (List.exists (fun coord -> List.mem coord ship_coords) coords)
+  then coords
+  else place_ship grid ship ship_coords
 
 let ywingCoords = place_ship randomgrid ywing []
 let xwingCoords = place_ship randomgrid xwing ywingCoords
 let mfalconCoords = place_ship randomgrid mfalcon (ywingCoords @ xwingCoords)
-let delta7Coords = place_ship randomgrid delta7 (ywingCoords @ xwingCoords @ mfalconCoords)
-let stardestroyerCoords = place_ship randomgrid stardestroyer (ywingCoords @ xwingCoords @ mfalconCoords @ delta7Coords)
+
+let delta7Coords =
+  place_ship randomgrid delta7 (ywingCoords @ xwingCoords @ mfalconCoords)
+
+let stardestroyerCoords =
+  place_ship randomgrid stardestroyer
+    (ywingCoords @ xwingCoords @ mfalconCoords @ delta7Coords)
+
 let place_ship_on_grid grid ship_coords =
   let update_grid row col =
     let row_arr = List.nth grid row in
@@ -110,8 +115,8 @@ let _ = place_ship_on_grid randomgrid delta7Coords
 let _ = place_ship_on_grid randomgrid stardestroyerCoords
 
 (*----------------------------------------------------------------------------------------------------------------
-   OPPOSING PLAYER MOVEMENT
-   --------------------------------------------------------------------------------------------------------------*)
+  OPPOSING PLAYER MOVEMENT
+  --------------------------------------------------------------------------------------------------------------*)
 
 let random_coord () =
   let random_col = Random.int 10 + 65 in
@@ -121,28 +126,34 @@ let random_coord () =
 let neighboring_points board =
   let neighbors (col, row) =
     let char_code = Char.code col in
-    let directions = [(-1, 0); (1, 0); (0, -1); (0, 1)] in
+    let directions = [ (-1, 0); (1, 0); (0, -1); (0, 1) ] in
     List.filter
       (fun (c, r) ->
-        r >= 0 && r <= 9 && c >= 65 && c <= 74 &&
-        (List.nth board r).(c - 65) <> "o" && (List.nth board r).(c - 65) <> "x")
+        r >= 0 && r <= 9 && c >= 65 && c <= 74
+        && (List.nth board r).(c - 65) <> "o"
+        && (List.nth board r).(c - 65) <> "x")
       (List.map (fun (dc, dr) -> (char_code + dc, row + dr)) directions)
   in
-  List.fold_left (fun acc row_arr ->
-    List.fold_left (fun inner_acc cell ->
-      let col, row = cell in
-      if (List.nth board row).(Char.code col - 65) = "x" then
-        inner_acc @ neighbors (col, row)
-      else
-        inner_acc
-    ) acc (Array.to_list row_arr)
-  ) [] (List.mapi (fun i row -> Array.mapi (fun j cell -> (Char.chr (j + 65), i)) row) board)
+  List.fold_left
+    (fun acc row_arr ->
+      List.fold_left
+        (fun inner_acc cell ->
+          let col, row = cell in
+          if (List.nth board row).(Char.code col - 65) = "x" then
+            inner_acc @ neighbors (col, row)
+          else inner_acc)
+        acc (Array.to_list row_arr))
+    []
+    (List.mapi
+       (fun i row -> Array.mapi (fun j cell -> (Char.chr (j + 65), i)) row)
+       board)
 
 let rec pick_random_point board =
   let available_neighbors = neighboring_points board in
   let col_code, row =
     if List.length available_neighbors > 0 then
-      List.nth available_neighbors (Random.int (List.length available_neighbors))
+      List.nth available_neighbors
+        (Random.int (List.length available_neighbors))
     else
       let col, row = random_coord () in
       (Char.code col, row)
@@ -151,7 +162,9 @@ let rec pick_random_point board =
   | "o" | "x" -> pick_random_point board
   | "#" ->
       (List.nth board row).(col_code - 65) <- "x";
-      (Char.chr col_code, row + 1, "x")  (* Return the updated coordinate and the symbol placed *)
+      (Char.chr col_code, row + 1, "x")
+      (* Return the updated coordinate and the symbol placed *)
   | _ ->
       (List.nth board row).(col_code - 65) <- "o";
-      (Char.chr col_code, row + 1, "o")  (* Return the updated coordinate and the symbol placed *)
+      (Char.chr col_code, row + 1, "o")
+(* Return the updated coordinate and the symbol placed *)
